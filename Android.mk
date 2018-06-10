6c66b123de36199385939e6fc2527715be9ba0db
Jaiglissechef-i9100 <yaggiland@gmail.com>
2018-06-10 23:22:16 +0200

    Refactor for O

--- Android.mk
+++ Android.mk
@@ -1,9 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v7-appcompat \
+    android-support-v14-preference \
+    android-support-v7-cardview \
+    android-support-design
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_SDK_VERSION := 27
 
 LOCAL_PACKAGE_NAME := aLogcat
 
